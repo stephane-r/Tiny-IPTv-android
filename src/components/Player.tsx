c7059b1dd18910e3e99bfe6421fca1128f68672d
Stéphane <contact@stephane-richin.fr>
2020-12-16 15:30:24 +0100

    :sparkles: Adding react-native-video

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Animated, StyleSheet } from 'react-native';
-import VlcPlayer from 'react-native-vlc-player';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, Dimensions, StatusBar, StyleSheet } from 'react-native';
+import Video from 'react-native-video';
 import { useAnimation } from 'react-native-animation-hooks';
 import { useApp } from '../states/app';
 import { TouchableNativeFeedback } from 'react-native-gesture-handler';
@@ -10,7 +10,16 @@ const INITIAL_PLAYER_DIMENSIONS = {
   height: 270,
 };
 
+const FULL_SCREEN_PLAYER_DIMENSIONS = {
+  width: Dimensions.get('window').width - 100,
+  height: Dimensions.get('window').height - StatusBar.currentHeight - 100,
+};
+
 const Player = () => {
+  const [playerDimensions, setPlayerDimensions] = useState(
+    INITIAL_PLAYER_DIMENSIONS,
+  );
+  const player = useRef();
   const app = useApp();
   const opacity = useAnimation({
     toValue: app.source ? 1 : 0.5,
@@ -22,6 +31,14 @@ const Player = () => {
     type: 'spring',
     useNativeDriver: true,
   });
+
+  const togglePlayerDimensions = () => {
+    if (playerDimensions.width === INITIAL_PLAYER_DIMENSIONS.width) {
+      return setPlayerDimensions(FULL_SCREEN_PLAYER_DIMENSIONS);
+    }
+
+    return setPlayerDimensions(INITIAL_PLAYER_DIMENSIONS);
+  };
 
   return (
     <Animated.View
@@ -37,16 +54,20 @@ const Player = () => {
         },
       ]}>
       {app.source ? (
-        <VlcPlayer
-          style={INITIAL_PLAYER_DIMENSIONS}
-          paused={false}
-          autoplay={true}
-          source={{
-            uri: app.source,
-            autoplay: true,
-            initOptions: ['--codec=avcodec'],
-          }}
-        />
+        <TouchableNativeFeedback onPress={togglePlayerDimensions}>
+          <Video
+            ref={player}
+            style={playerDimensions}
+            paused={false}
+            autoplay={true}
+            resizeMode="cover"
+            source={{
+              uri: app.source,
+              type: 'mpeg',
+            }}
+            onError={console.log}
+          />
+        </TouchableNativeFeedback>
       ) : null}
     </Animated.View>
   );
@@ -55,10 +76,10 @@ const Player = () => {
 const styles = StyleSheet.create({
   playerContainer: {
     position: 'absolute',
-    right: 0,
-    bottom: 0,
+    right: 50,
+    bottom: 50,
     zIndex: 2,
-    padding: 50,
+    backgroundColor: 'red',
   },
 });
 
