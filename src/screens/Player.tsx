37f555be4572544983338c78a4afc80963eab6c1
Stéphane <contact@stephane-richin.fr>
2020-12-19 14:45:18 +0100

    :iphone: Adding test for show player screen only for landscape view

--- src/screens/Player.tsx
+++ src/screens/Player.tsx
@@ -1,18 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dimensions, StatusBar, View } from 'react-native';
 import { useIsFocused, useRoute } from '@react-navigation/native';
 import VLCPlayer from 'react-native-vlc-player';
-import { isTablet } from 'react-native-device-info';
 import { setSource } from '../states/app';
+import { Chip } from 'react-native-paper';
+import { isTablet } from 'react-native-device-info';
+
+const isPortrait = () => {
+  const dim = Dimensions.get('window');
+  return Boolean(dim.height >= dim.width);
+};
 
 const PlayerScreen = () => {
   const { params } = useRoute();
   const isFocused = useIsFocused();
+  const [isLandscape, setIsLandscape] = useState(!isPortrait());
 
   useEffect(() => {
     setSource({
       uri: null,
       visible: false,
+    });
+
+    Dimensions.addEventListener('change', (event) => {
+      const { window } = event;
+      setIsLandscape(window.width > window.height);
     });
 
     return () =>
@@ -24,7 +36,7 @@ const PlayerScreen = () => {
 
   return (
     <View style={{ flex: 1 }}>
-      {isFocused && (
+      {isLandscape && isFocused ? (
         <VLCPlayer
           style={{
             width: Dimensions.get('window').width,
@@ -40,6 +52,10 @@ const PlayerScreen = () => {
             initOptions: ['--codec=avcodec'],
           }}
         />
+      ) : (
+        <Chip icon="information" style={{ justifyContent: 'center' }}>
+          Please rotate your device
+        </Chip>
       )}
     </View>
   );
