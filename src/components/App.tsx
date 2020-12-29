d2999ecd95a0eeccd45eda57a98907e03e126e29
Stéphane <contact@stephane-richin.fr>
2020-12-29 08:39:43 +0100

    :sparkles: Enabled react-native-keep-awake

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,19 +2,22 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Screen } from '../enums/Screen';
-import LoadingScreen from '../screens/Loading';
-import LoginScreen from '../screens/Login';
-import HomeScreen from '../screens/Home';
 import {
   DarkTheme,
   DefaultTheme,
   Provider as PaperProvider,
 } from 'react-native-paper';
+import KeepAwake from 'react-native-keep-awake';
+import { Screen } from '../enums/Screen';
+import LoadingScreen from '../screens/Loading';
+import LoginScreen from '../screens/Login';
+import HomeScreen from '../screens/Home';
 import Snackbar from './Snackbar';
 import useUpdateRelease from '../hooks/useUpdateRelease';
 import SettingsScreen from '../screens/Settings';
 import { Appearance } from 'react-native';
+
+KeepAwake.activate();
 
 const Stack = createStackNavigator();
 
